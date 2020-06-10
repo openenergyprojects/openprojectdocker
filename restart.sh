ba5db93049c1033cea8ad4ad673a8164ff1f0f49
Open Energy Projects - DC NEXT Eve <openenergyprojects@gmail.com>
2020-06-10 23:46:27 +0300

    STORAGE_FULL_PATH

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+STORAGE_FULL_PATH=/srv/docker/openprojectdocker #don't use relative path, as it will create docker volumes!
 #IMG=openproject/community:10.4.1
 IMG=openproject/community:10.6.0
 docker pull $IMG
@@ -8,10 +9,10 @@ docker stop openprojectdocker || true
 
 docker rm -f openprojectdocker || true
 docker run -d -p 14080:80 --restart always --name openprojectdocker \
-  --env-file /srv/docker/openprojectdocker/docker.env \
-  -v /srv/docker/openprojectdocker/pgdata:/var/openproject/pgdata \
-  -v /srv/docker/openprojectdocker/static:/var/openproject/assets \
-  -v /srv/docker/openprojectdocker/production.rb:/app/config/environments/production.rb \
+  --env-file ${STORAGE_FULL_PATH}/docker.env \
+  -v ${STORAGE_FULL_PATH}/pgdata:/var/openproject/pgdata \
+  -v ${STORAGE_FULL_PATH}/static:/var/openproject/assets \
+  -v ${STORAGE_FULL_PATH}/production.rb:/app/config/environments/production.rb \
   $IMG
 
 #sleep 20
